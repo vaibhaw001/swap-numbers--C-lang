f3915d1feb070e2e684d77f118e626cb089e95d6
vaibhaw <68787391+vaibhaw001@users.noreply.github.com>
2020-08-03 00:11:43 +0530

    Update NEW8.C

--- NEW8.C
+++ NEW8.C
@@ -1,3 +1,6 @@
+// hello this is my first c program please support and avoid my mistakes
+// i am going to be a coder 5 years down the line.
+
 #include <stdio.h>
 void main() {
        int first, second, temp;
